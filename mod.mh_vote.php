00797520ac55864a8dd9b5d277dc64b712140a98
Mark Huot <mark@markhuot.com>
2010-02-17 16:17:08 -0500

    removed a bunch of methods and got it working on a single :stats method

--- mod.mh_vote.php
+++ mod.mh_vote.php
@@ -4,6 +4,14 @@ require_once PATH_THIRD.'mh/mh'.EXT;
 
 class Mh_vote {
 
+
+
+
+	public $me = array();
+	
+	
+	
+	
 	public function votes()
 	{
 		mh()->db->where('unique_id', mh()->TMPL->fetch_param('id'));
@@ -69,92 +77,76 @@ class Mh_vote {
 	
 	
 	
-	public function trailing()
+	public function stats()
 	{
 		$tagdata = mh()->TMPL->tagdata;
 		
-		$id = mh()->TMPL->fetch_param('id');
-		$me = mh()->db->get_where('exp_mh_vote_meta', array('unique_id' => $id))->row();
+		$id = $this->_id(mh()->TMPL->tagparams);
+		$me = $this->_me($id);
 		
 		if (!$me)
 		{
-			return '';
+			return mh()->TMPL->parse_variables($tagdata, array(array()));
 		}
 		
-		mh()->db->where('count > '.$me->count);
-		mh()->db->orderby('count asc');
-		mh()->db->limit(1);
-		$result = mh()->db->get('mh_vote_meta')->row();
-		
-		if ($result)
-		{
-			return mh()->TMPL->parse_variables($tagdata, array(array(
-				'id' => $result->unique_id,
-				'trailing_by' => $result->count-$me->count,
-				'pluralize' => $me->count-$result->count>1?'s':''
-			)));
-		}
-		
-		else
-		{
-			$no_results_match = LD.'if is_first'.RD.'(.*?)'.LD.'\/if'.RD;
-			if (preg_match('/'.$no_results_match.'/sm', $tagdata))
-			{
-				$tagdata = preg_replace('/^.*'.$no_results_match.'?.*$/sm', '$1', $tagdata);
-			}
-			else
-			{
-				$tagdata = '';
-			}
-		}
-		
-		return $tagdata;
-	}
-	
-	
-	
-	
-	public function leading()
-	{
-		$tagdata = mh()->TMPL->tagdata;
-		
-		$id = mh()->TMPL->fetch_param('id');
-		$me = mh()->db->get_where('exp_mh_vote_meta', array('unique_id' => $id))->row();
-		
-		if (!$me)
-		{
-			return '';
-		}
-		
-		mh()->db->where('count < '.$me->count);
 		mh()->db->orderby('count desc');
-		mh()->db->limit(1);
-		$result = mh()->db->get('mh_vote_meta')->row();
+		$places = mh()->db->get('mh_vote_meta');
+		$results = $places->result();
 		
-		if ($result)
+		$leading = $this->_leading($results, $id);
+		$trailing = $this->_trailing($results, $id);
+		$tied = $this->_tied($results, $id);
+		list($place, $total_places) = $this->_place($results, $id);
+		
+		foreach (array('leading', 'trailing', 'tied') as $dir)
 		{
-			return mh()->TMPL->parse_variables($tagdata, array(array(
-				'id' => $result->unique_id,
-				'leading_by' => $me->count-$result->count,
-				'pluralize' => $me->count-$result->count>1?'s':''
-			)));
+			$adjacent = $$dir;
+			
+		 	preg_match_all('/'.LD.'mh_vote:'.$dir.'(.*?)'.RD.'(.*?)'.LD.'\/mh_vote:'.$dir.RD.'/sm', $tagdata, $act_tagdata);
+		 	if ($act_tagdata)
+		 	{
+		 		foreach ($act_tagdata[0] as $key => $full_match)
+		 		{
+		 			$params = mh()->functions->assign_parameters($act_tagdata[1][$key]);
+		 			
+		 			$parsed = '';
+		 			foreach ($adjacent as $adjacent_votee)
+		 			{
+		 				$parsed.= mh()->TMPL->parse_variables($act_tagdata[2][$key], array($this->_data(@$adjacent_votee->unique_id)));
+		 			}
+		 			
+		 			$parsed = trim($parsed);
+		 			
+		 			if (isset($params['backspace']) && is_numeric($params['backspace']))
+		 			{
+		 				$parsed = substr($parsed, 0, strlen($parsed)-$params['backspace']);
+		 			}
+		 			
+		 			$tagdata = str_replace($full_match, $parsed, $tagdata);
+		 		}
+		 	}
 		}
 		
-		else
-		{
-			$no_results_match = LD.'if is_last'.RD.'(.*?)'.LD.'\/if'.RD;
-			if (preg_match('/'.$no_results_match.'/sm', $tagdata))
-			{
-				$tagdata = preg_replace('/^.*'.$no_results_match.'?.*$/sm', '$1', $tagdata);
-			}
-			else
-			{
-				$tagdata = '';
-			}
-		}
 		
+		$leading_by = $me->count - @$leading[0]->count;
+		$trailing_by = @$trailing[0]->count - $me->count;
+		
+		$tagdata = mh()->TMPL->parse_variables($tagdata, array(array(
+			'mh_vote:place' => $place,
+			'mh_vote:is_tied' => count($tied)>0,
+			'mh_vote:suffix' => date('S', strtotime("2000-01-{$place}")),
+			'mh_vote:votes' => $me->count,
+			'mh_vote:is_first' => $place==1,
+			'mh_vote:leading_by' => $leading_by>0?$leading_by:0,
+			'mh_vote:leading_count' => count($leading),
+			'mh_vote:is_last' => $place==$total_places,
+			'mh_vote:trailing_by' => $trailing_by>0?$trailing_by:0,
+			'mh_vote:trailing_count' => count($trailing),
+			'mh_vote:to_lead' => $trailing_by>0?$trailing_by+1:0
+		)));
+
 		return $tagdata;
-	}
+	}	
 	
 	
 	
@@ -191,20 +183,23 @@ class Mh_vote {
 			return mh()->output->fatal_error(mh()->lang->line('captcha_incorrect'));
 		}
 		
+		// generate id
+		$id = $this->_id($params);
+		
 		// insert vote
 		mh()->db->insert('exp_mh_votes', array(
-			'unique_id' => $params['id'],
+			'unique_id' => $id,
 			'vote_date' => time(),
 			'voter_ip' => mh()->input->ip_address(),
 			'voter_useragent' => mh()->input->server('HTTP_USER_AGENT')
 		));
 		
 		// update meta
-		if (($count = mh()->db->where('unique_id', $params['id'])->count_all_results('mh_votes')) == 1)
+		if (($count = mh()->db->where('unique_id', $id)->count_all_results('mh_votes')) == 1)
 		{
 			mh()->db->insert('exp_mh_vote_meta', array(
 				'count' => $count,
-				'unique_id' => $params['id']
+				'unique_id' => $id
 			));
 		}
 		
@@ -212,25 +207,21 @@ class Mh_vote {
 		{
 			mh()->db->update('exp_mh_vote_meta', array(
 				'count' => $count
-			), array('unique_id' => $params['id']));
+			), array('unique_id' => $id));
 		}
 		
 		// store it somewhere?
-		if (($store_in = @$params['store_in']) && ($store_key = @$params['store_key']))
+		if (@$params['store_in'])
 		{
-			$store_with = @$params['store_with']?$params['store_with']:'entry';
-			
-			switch ($store_with)
+			if (substr($id, 0, 1) == 'm')
 			{
-				case 'member':
-					$field_id = mh()->db->get_where('exp_member_fields', array('m_field_name' => $store_in))->row('m_field_id');
-					mh()->db->update('exp_member_data', array('m_field_id_'.$field_id => $count), array('member_id' => $store_key));
-					break;
-				
-				case 'entry':
-					$field_id = mh()->db->get_where('exp_channel_fields', array('field_name' => $store_in))->row('field_id');
-					mh()->db->update('exp_channel_data', array('field_id_'.$field_id => $count), array('entry_id' => $store_key));
-					break;
+				$field_id = mh()->db->get_where('exp_member_fields', array('m_field_name' => $params['store_in']))->row('m_field_id');
+				mh()->db->update('exp_member_data', array('m_field_id_'.$field_id => $count), array('member_id' => substr($id, 1)));
+			}
+			else
+			{
+				$field_id = mh()->db->get_where('exp_channel_fields', array('field_name' => $params['store_in']))->row('field_id');
+				mh()->db->update('exp_channel_data', array('field_id_'.$field_id => $count), array('entry_id' => substr($id, 1)));
 			}
 		}
 		
@@ -303,5 +294,169 @@ class Mh_vote {
 		mh()->db->where("vote_date > {$min}");
 		return mh()->db->get('exp_mh_votes')->num_rows==0;
 	}
+	
+	
+	
+	
+	public function _data($unique_id)
+	{
+		if (!$unique_id) return array();
+		
+		$id = substr($unique_id, 1);
+		switch (substr($unique_id, 0, 1) == 'm')
+		{
+			case 'm':
+				mh()->db->join('exp_member_data', 'exp_member_data.member_id=exp_members.member_id', 'LEFT');
+				$row = mh()->db->get_where('exp_members', array('exp_members.member_id' => $id))->row_array();
+				break;
+			
+			case 'e':
+				mh()->db->join('exp_channel_data', 'exp_channel_data.entry_id=exp_channel_titles.entry_id', 'LEFT');
+				$row = mh()->db->get_where('exp_channel_title', array('exp_channel_title.entry_id' => $id))->row_array();
+				break;
+		}
+		
+		foreach (array_keys($row) as $key)
+		{
+			$row["mh_vote:{$key}"] = $row[$key];
+			unset($row[$key]);
+		}
+		
+		return $row;
+	}
+	
+	
+	
+	
+	public function _id($params)
+	{
+		if (@$params['member_id']) return 'm'.$params['member_id'];
+		
+		return 'e'.@$params['entry_id'];
+	}
+	
+	
+	
+	
+	public function _me($id)
+	{
+		if (!isset($this->me[$id]))
+		{
+			$this->me[$id] = mh()->db->get_where('exp_mh_vote_meta', array('unique_id' => $id))->row();
+		}
+		
+		return $this->me[$id];
+	}
+	
+	
+	
+	
+	public function _place($results, $id)
+	{
+		$place = 1;
+		$my_place = 1;
+		
+		foreach ($results as $count => $result)
+		{
+			if (isset($results[$count-1]) && $result->count < $results[$count-1]->count)
+			{
+				$place++;
+			}
+			
+			if ($result->unique_id == $id)
+			{
+				$my_place = $place;
+			}
+		}
+		
+		return array($my_place, $place);
+	}
+	
+	
+	
+	
+	public function _trailing($results, $id)
+	{
+		foreach ($results as $count => $result)
+		{
+			if ($result->unique_id == $id)
+			{
+				while (isset($results[$count]) && $results[$count]->count == $result->count)
+				{
+					$count--;
+				}
+				
+				if (isset($results[$count]))
+				{
+					return array_merge(array($results[$count]), $this->_tied($results, @$results[$count]->unique_id));
+				}
+			}
+		}
+		
+		return array();
+	}
+	
+	
+	
+	
+	public function _leading($results, $id)
+	{
+		foreach ($results as $count => $result)
+		{
+			if ($result->unique_id == $id)
+			{
+				while (isset($results[$count]) && $results[$count]->count == $result->count)
+				{
+					$count++;
+				}
+				
+				if (isset($results[$count]))
+				{
+					return array_merge(array($results[$count]), $this->_tied($results, @$results[$count]->unique_id));
+				}
+			}
+		}
+		
+		return array();
+	}
+	
+	
+	
+	
+	public function _tied($results, $id)
+	{
+		if (!$id) return array();
+		
+		$tied = array();
+		
+		foreach ($results as $count => $me)
+		{
+			if ($me->unique_id == $id)
+			{
+				foreach ($results as $result)
+				{
+					if ($result->count == $me->count && $result->unique_id != $me->unique_id)
+					{
+						$tied[] = $result;
+					}
+				}
+				
+				break;
+			}
+		}
+		
+		return $tied;
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
